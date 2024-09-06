5c1857e2a84db9d07f2992c04ea60182fdb78413
nav singh <navtej927@gmail.com>
2024-09-06 21:44:13 +0000

    terraform updated

--- main.tf
+++ main.tf
@@ -13,12 +13,24 @@ provider "aws" {
 #   }
 # }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "public-ec2" {
   ami           = "ami-0182f373e66f89c85"  # Replace with your desired AMI ID
   instance_type = "t2.micro"               # Specify the instance type
   key_name      = "navt92-keypair" # Replace with your existing key pair name
-
+  subnet_id = "subnet-0e4b8099ab31479e5"
+  vpc_security_group_ids = ["sg-06bde5ddcc504e95a"]
   tags = {
-    Name = "ec2-delete"
+    Name = "ec2-public-delete"
+  }
+}
+
+resource "aws_instance" "private-ec2" {
+  ami           = "ami-0182f373e66f89c85"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"               # Specify the instance type
+  key_name      = "navt92-keypair" # Replace with your existing key pair name
+  subnet_id = "subnet-0d1cc329b23e45c82"
+  vpc_security_group_ids = ["sg-06bde5ddcc504e95a"]
+  tags = {
+    Name = "ec2-private-delete"
   }
 }
