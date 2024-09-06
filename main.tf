390698df5bdebf371a421e3da5e5a5d665078db9
nav singh <navtej927@gmail.com>
2024-09-06 18:04:04 +0000

    terraform for ec2

--- main.tf
+++ main.tf
@@ -3,12 +3,22 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "mproddevdelete"
-  acl    = "private"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "mproddevdelete"
+#   acl    = "private"
+
+#   tags = {
+#     Name        = "My Example Bucket"
+#     Environment = "Dev"
+#   }
+# }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0182f373e66f89c85"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"               # Specify the instance type
+  key_name      = "navt92-keypair" # Replace with your existing key pair name
 
   tags = {
-    Name        = "My Example Bucket"
-    Environment = "Dev"
+    Name = "ec2-delete"
   }
 }
